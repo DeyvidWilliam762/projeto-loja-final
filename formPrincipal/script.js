ef1b2912db724f185b912b827a5fe5974c961b64
Deyvid William <110306736+DeyvidWilliam762@users.noreply.github.com>
2023-07-04 19:00:06 -0300

    tela de login 90% finalizada

--- formPrincipal/script.js
+++ formPrincipal/script.js
@@ -6,10 +6,12 @@ const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 const btnValidate = document.getElementById("txtValidar");
 const ErroAviso = document.getElementById("#LabelBTN");
 const ValidandoForm = document.querySelector("msgVld");
+const msgCreate= document.getElementById('elmSpan')
 
 const usuario = localStorage.getItem("usuario")
 const email = localStorage.getItem("email");
 const senha = localStorage.getItem("senha");
+
 
 
 
@@ -28,8 +30,11 @@ forms.addEventListener("submit", (event) => {
     localStorage.setItem("usuario", JSON.stringify(campos[0].value));
     localStorage.setItem("email", JSON.stringify(campos[1].value));
     localStorage.setItem("senha", JSON.stringify(campos[2].value));
+    msgCreate.innerHTML = '<h1>Conta criada!!</h1>'
 
 
+  }else{
+    msgCreate.innerHTML = '<h1 style="color: red;"> Campo incorreto</h1>'
   }
 
 
@@ -80,6 +85,8 @@ function comparePassword() {
   }
 }
 
+
+
 /*validar usuario e senha */
 
 
@@ -89,16 +96,16 @@ const formLogin = document.getElementById("formLogin");
 const senhaLG = document.getElementById('senha')
 const usuarioLG = document.getElementById('usuario')
 
-const urlLojaPG = 
 
 
 
 
-formLogin.addEventListener("submit", (event) => {
-  event.preventDefault();
-  validarCamposLogin();
 
-});
+  formLogin.addEventListener("submit", (event) => {
+    event.preventDefault();
+    validarCamposLogin();
+
+  });
 
 
 function LogError() {
@@ -118,9 +125,9 @@ function logOK() {
 function validarCamposLogin() {
   if (usuarioLG.value === JSON.parse(usuario) && (senhaLG.value === JSON.parse(senha))) {
     console.log('ok')
+    logOK()
   } else {
-    console.log(JSON.parse(usuario))
-    console.log(usuarioLG.value)
+    LogError()
   }
 
 
